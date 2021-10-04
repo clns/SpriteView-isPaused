a397797bd829a6898a473ff7569f04b7d4a2a12a
Calin <calinseciu@gmail.com>
2021-10-04 16:18:23 -0600

    Fixed version using `ObservableObject` and `StateObject`
    As suggested:
    
    - https://developer.apple.com/forums/thread/691607
    - https://stackoverflow.com/questions/69442079/pause-a-spritekit-scene-from-swiftui-using-spriteviewispaused-without-reinit
    
    The timer was replaced with a simple "Updates: X" increment, per @jnpdx's suggestion.
    
    Also the .sheet() is set to use a transparent background, so the scene behind it can be seen and the paused state observed.

--- SpriteView-isPaused/ContentView.swift
+++ SpriteView-isPaused/ContentView.swift
@@ -1,35 +1,35 @@
 import SwiftUI
 import SpriteKit
 
-class GameScene: SKScene {
-    private let label = SKLabelNode(text: "Time Elapsed:\n0")
-    private var lastUpdateTime : TimeInterval = 0
+class GameScene: SKScene, ObservableObject {
+    private let label = SKLabelNode(text: "Updates:\n0")
+    private var updates = 0
     
     override func didMove(to view: SKView) {
         addChild(label)
+        label.numberOfLines = 2
     }
     
     override func update(_ currentTime: TimeInterval) {
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        let seconds = Int(currentTime - lastUpdateTime)
-        label.text = "Time Elapsed:\n\(seconds)"
-        label.numberOfLines = 2
+        updates += 1
+        label.text = "Updates:\n\(updates)"
+    }
+    
+    func isPaused(_ isPaused: Bool) {
+        print("isPaused(\(isPaused))")
     }
 }
 
 struct ContentView: View {
     @State private var showingLevelChooser = false
     
-    var scene: SKScene {
+    @StateObject var scene: GameScene = {
         let scene = GameScene()
         scene.size = CGSize(width: 300, height: 400)
         scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         scene.scaleMode = .fill
         return scene
-    }
+    }()
     
     var body: some View {
         ZStack {
@@ -47,10 +47,31 @@ struct ContentView: View {
                 Button("Cancel") {
                     showingLevelChooser.toggle()
                 }
-                Text("Level Chooser")
+                Spacer()
             }
+            .background(BackgroundClearView())
+        }
+        .onChange(of: showingLevelChooser) {
+            scene.isPaused($0)
         }
     }
+}
+
+/*
+ Make the .sheet() transparent, so we can see the scene behind.
+ 
+ Idea from https://stackoverflow.com/questions/63745084/how-can-i-make-a-background-color-with-opacity-on-a-sheet-view
+ */
+struct BackgroundClearView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
